6b4c7dc5d13f4a2a535151963b5849f2369362f7
astroash <astroaisha@gmail.com>
2017-08-02 18:17:47 +0100

    created basic dropdown for site using css & html :lipstick: relates #20 #19

---
+++ public/dom.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+var dropDown = document.getElementById('drop-down-btn');
+dropDown.addEventListener("click", function(){
+    document.getElementById("myDropdown").classList.toggle("show");
+});
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
