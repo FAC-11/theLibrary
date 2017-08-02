c0eb80af7e18d6ae988ac56519cd0fce94702388
astroash <astroaisha@gmail.com>
2017-08-02 15:46:04 +0100

    tested all public routes :heart: relates #9 #10

--- tests/src.test.js
+++ tests/src.test.js
@@ -7,9 +7,18 @@ test('initial setup on src', (t) => {
   t.end();
 });
 
-test('checking home route', (t) => {
+test('checking route and handlers', (t) => {
   shot.inject(router, { method: 'get', url: '/' }, (res) => {
-    t.equal(res.statusCode, 200, 'should respond with status code 200');
+    t.equal(res.statusCode, 200, '/ should respond with status code 200');
+  });
+  shot.inject(router, { method: 'get', url: 'public/style.css' }, (res) => {
+    t.equal(res.statusCode, 200, 'public/style.css should respond with status code 200');
+  });
+  shot.inject(router, { method: 'get', url: 'public/request.js' }, (res) => {
+    t.equal(res.statusCode, 200, 'public/request.js should respond with status code 200');
+  });
+  shot.inject(router, { method: 'get', url: 'public/favicon.ico' }, (res) => {
+    t.equal(res.statusCode, 200, 'public/favicon.ico should respond with status code 200');
     t.end();
   });
 });
