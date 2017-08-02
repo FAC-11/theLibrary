dcd11a8b16eb3b7df3e810c1cbb74cc51f583704
astroash <astroaisha@gmail.com>
2017-08-02 15:12:18 +0100

    created handlers function which serves the correct files for public folders and holds spaces for calling the correct handleTopic/ handleTrending functions :hankey: relates #10

--- src/router.js
+++ src/router.js
@@ -2,22 +2,18 @@ const handlers = require('./handlers');
 
 const router = (req, res) => {
   const url = req.url;
-  if (url === "/") {
+  if (url === '/') {
     handlers.handleHomeRoute(req, res);
-  }
-  else if (url.includes('public')) {
+  } else if (url.includes('public')) {
     handlers.handlePublic(req, res, url);
-  }
-  else if (url.includes('?topic')) {
+  } else if (url.includes('?topic')) {
     handlers.handleTopic(req, res, url);
-  }
-  else if (url.includes('?trending')) {
+  } else if (url.includes('?trending')) {
     handlers.handleTrending(req, res, url);
-  }
-  else {
+  } else {
     res.writeHead(404, 'Content-Type: text/html');
     res.end('<h1>404 not found</h1>');
   }
-}
+};
 
 module.exports = router;
