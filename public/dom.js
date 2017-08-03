3cd169fcab084bb8d51ecf5bacaf0ce228d4e1f7
Max Gerber <mgerber94@gmail.com>
2017-08-03 12:57:45 +0100

    created basic format for results table with styling Relates #15 and #20

--- public/dom.js
+++ public/dom.js
@@ -3,9 +3,21 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 var dropDown = document.getElementById('drop-down-btn');
+var resultsTable = document.getElementById('results-table');
 dropDown.addEventListener("click", function(){
     document.getElementById("myDropdown").classList.toggle("show");
 });
+
+// Render the DOM - our callback from request.js
+var DOMRender = function(err, res) {
+  if (err) {
+    console.log(err);
+    // output something funky on error
+  } else {
+    resultsTable.innerHTML = '';
+    var stuff = '';
+  }
+}
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
